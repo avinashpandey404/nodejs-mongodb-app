b28e017d566fac795a1733b8e5923b8084cdad05
avinashpandey404 <160997057+avinashpandey404@users.noreply.github.com>
2026-04-30 13:52:00 +0530

    Update auth.js

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -1,15 +1,17 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  const token = req.headers.authorization;
+  const token = req.headers.authorization?.split(' ')[1];
 
-  if (!token) return res.status(401).json({ message: 'No token' });
+  if (!token) {
+    return res.status(401).json({ message: 'No token' });
+  }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded.id;
+    req.user = decoded;
     next();
-  } catch {
+  } catch (err) {
     res.status(401).json({ message: 'Invalid token' });
   }
 };
