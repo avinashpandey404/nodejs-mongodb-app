a9e05f3b4386f7ef616244d8ad5bf43a6e079e89
avinashpandey404 <160997057+avinashpandey404@users.noreply.github.com>
2026-04-30 13:43:08 +0530

    Update User.js

--- app/models/User.js
+++ app/models/User.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
-});
+  name: String,
+  email: { type: String, unique: true },
+  password: String,
+  role: { type: String, default: 'user' }
+}, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
