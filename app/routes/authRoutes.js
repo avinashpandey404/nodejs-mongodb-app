3cfa711f98827801bcfda92fce84c1ab07636c5c
avinashpandey404 <160997057+avinashpandey404@users.noreply.github.com>
2026-04-30 13:48:59 +0530

    Update authRoutes.js

--- app/routes/authRoutes.js
+++ app/routes/authRoutes.js
@@ -1,5 +1,4 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
 const auth = require('../controllers/authController');
 
 router.post('/register', auth.register);
