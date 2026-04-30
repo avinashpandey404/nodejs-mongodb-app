38f7b76b80371b6df87daa84c2bececfb6d66e10
avinashpandey404 <160997057+avinashpandey404@users.noreply.github.com>
2026-04-30 12:26:13 +0530

    Create authController.js

---
+++ app/controllers/authController.js
@@ -0,0 +1,32 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const generateToken = require('../utils/generateToken');
+
+exports.register = async (req, res) => {
+  const { email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists) return res.status(400).json({ message: 'User exists' });
+
+  const hashed = await bcrypt.hash(password, 10);
+
+  const user = await User.create({ email, password: hashed });
+
+  res.json({
+    token: generateToken(user._id)
+  });
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) return res.status(400).json({ message: 'Invalid credentials' });
+
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) return res.status(400).json({ message: 'Invalid credentials' });
+
+  res.json({
+    token: generateToken(user._id)
+  });
+};
